de64e2a2f8d4d72613e674fb528dd3fa34354212
Jonah Starling <starling.jonah@gmail.com>
2020-02-26 22:54:04 -0600

    It Works
    Copied some of the bitmap and byte buffer handling code from tensorflow's example they just released. Definitely need to do a lot of code clean up next before starting on some bugs and new features. One cool side effect I found was since I can do the style transfer mutliple times the image can be made to look more and more like the original (relatively).

--- app/src/main/java/com/jonahstarling/pastiche/tflite/ArtisticStyleTransfer.kt
+++ app/src/main/java/com/jonahstarling/pastiche/tflite/ArtisticStyleTransfer.kt
@@ -8,69 +8,38 @@ import com.jonahstarling.pastiche.R
 import com.jonahstarling.pastiche.utility.BitmapHelper
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.Interpreter
-import org.tensorflow.lite.support.image.ops.ResizeOp
-import org.tensorflow.lite.support.image.ImageProcessor
-import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import java.io.IOException
 import java.nio.MappedByteBuffer
 import java.nio.channels.FileChannel.MapMode.READ_ONLY
-import org.tensorflow.lite.support.common.ops.NormalizeOp
-import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp
 import java.io.FileInputStream
 import java.nio.ByteBuffer
 
 class ArtisticStyleTransfer(private val context: Context, private val contentBitmap: Bitmap) {
 
-    private val contentImage: TensorImage
-        get() = convertContentImageToBitmap()
-
     private val bitmapHelper = BitmapHelper()
 
     fun demo(): Bitmap? {
-        val styleImage = resizeStyleImage()
+        val styleImageBitmap = BitmapFactory.decodeResource(context.resources,
+            R.drawable.starry_night
+        )
+        val styleImage = bitmapHelper.bitmapToByteBuffer(styleImageBitmap, 256, 256)
         val styleBottleneck = runStylePredictionModel(styleImage)
+        val contentByteBuffer = bitmapHelper.bitmapToByteBuffer(contentBitmap, 384, 384)
         return if (styleBottleneck != null) {
-            runStyleTransferModel(styleBottleneck)
+            runStyleTransferModel(contentByteBuffer, styleBottleneck)
         } else {
             Log.e(TAG, "Error getting style bottleneck.")
             null
         }
     }
 
-    private fun convertContentImageToBitmap(): TensorImage {
-        val cropSize = Math.min(contentBitmap.width, contentBitmap.height)
-        val imageProcessor = ImageProcessor.Builder()
-            .add(ResizeWithCropOrPadOp(cropSize, cropSize))
-            .add(ResizeOp(384, 384, ResizeOp.ResizeMethod.BILINEAR))
-            .add(NormalizeOp(0.0f, 1.0f))
-            .build()
-        var contentImage = TensorImage(DataType.FLOAT32)
-        contentImage.load(contentBitmap)
-        contentImage = imageProcessor.process(contentImage)
-        return contentImage
-    }
-
-    private fun resizeStyleImage(): TensorImage {
-        val imageProcessor = ImageProcessor.Builder()
-            .add(ResizeOp(256, 256, ResizeOp.ResizeMethod.BILINEAR))
-            .add(NormalizeOp(0.0f, 1.0f))
-            .build()
-        var styleImage = TensorImage(DataType.FLOAT32)
-        val styleImageBitmap = BitmapFactory.decodeResource(context.resources,
-            R.drawable.starry_night
-        )
-        styleImage.load(styleImageBitmap)
-        styleImage = imageProcessor.process(styleImage)
-        return styleImage
-    }
-
-    private fun runStylePredictionModel(styleImage: TensorImage): TensorBuffer? {
+    private fun runStylePredictionModel(styleImage: ByteBuffer): TensorBuffer? {
         loadMappedByteBufferFromAssets("style_predict_quantized_256.tflite")?.let { stylePredictModel ->
             val interpreter = Interpreter(stylePredictModel)
             val styleBottleneck = TensorBuffer.createFixedSize(intArrayOf(1, 1, 100), DataType.FLOAT32)
 
-            interpreter.run(styleImage.tensorBuffer.buffer, styleBottleneck.buffer)
+            interpreter.run(styleImage, styleBottleneck.buffer)
 
             return styleBottleneck
         } ?: run {
@@ -78,16 +47,17 @@ class ArtisticStyleTransfer(private val context: Context, private val contentBit
         }
     }
 
-    private fun runStyleTransferModel(styleBottleneck: TensorBuffer): Bitmap? {
+    private fun runStyleTransferModel(contentByteBuffer: ByteBuffer, styleBottleneck: TensorBuffer): Bitmap? {
         loadMappedByteBufferFromAssets("style_transfer_quantized_dynamic.tflite")?.let { styleTransferModel ->
             val interpreter = Interpreter(styleTransferModel)
 
-            val input = arrayOf(contentImage.tensorBuffer.buffer, styleBottleneck.buffer)
-            val output = mapOf(Pair(0, TensorBuffer.createFixedSize(intArrayOf(384, 384, 3), DataType.FLOAT32).buffer))
+            val input = arrayOf(contentByteBuffer, styleBottleneck.buffer)
+            val output = HashMap<Int, Any>()
+            val outputArray = Array(1) { Array(384) { Array(384) { FloatArray(3) } } }
+            output[0] = outputArray
             interpreter.runForMultipleInputsOutputs(input, output)
 
-            val outputByteBuffer = output[0] as ByteBuffer
-            return bitmapHelper.bufferToBitmap(outputByteBuffer, 384, 384)
+            return bitmapHelper.convertArrayToBitmap(outputArray, 384, 384)
         } ?: run {
             return null
         }
--- app/src/main/java/com/jonahstarling/pastiche/utility/BitmapHelper.kt
+++ app/src/main/java/com/jonahstarling/pastiche/utility/BitmapHelper.kt
@@ -1,10 +1,10 @@
 package com.jonahstarling.pastiche.utility
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Matrix
+import android.graphics.*
 import android.media.Image
 import java.nio.Buffer
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 class BitmapHelper {
     
@@ -45,5 +45,89 @@ class BitmapHelper {
         val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         bitmap.copyPixelsFromBuffer(buffer)
         return bitmap
+    }
+
+    private fun scaleBitmapAndKeepRatio(
+        targetBmp: Bitmap,
+        reqHeightInPixels: Int,
+        reqWidthInPixels: Int
+    ): Bitmap {
+        if (targetBmp.height == reqHeightInPixels && targetBmp.width == reqWidthInPixels) {
+            return targetBmp
+        }
+        val matrix = Matrix()
+        matrix.setRectToRect(
+            RectF(
+                0f, 0f,
+                targetBmp.width.toFloat(),
+                targetBmp.width.toFloat()
+            ),
+            RectF(
+                0f, 0f,
+                reqWidthInPixels.toFloat(),
+                reqHeightInPixels.toFloat()
+            ),
+            Matrix.ScaleToFit.FILL
+        )
+        return Bitmap.createBitmap(
+            targetBmp, 0, 0,
+            targetBmp.width,
+            targetBmp.width, matrix, true
+        )
+    }
+
+    fun bitmapToByteBuffer(
+        bitmapIn: Bitmap,
+        width: Int,
+        height: Int,
+        mean: Float = 0.0f,
+        std: Float = 255.0f
+    ): ByteBuffer {
+        val bitmap = scaleBitmapAndKeepRatio(bitmapIn, width, height)
+        val inputImage = ByteBuffer.allocateDirect(1 * width * height * 3 * 4)
+        inputImage.order(ByteOrder.nativeOrder())
+        inputImage.rewind()
+
+        val intValues = IntArray(width * height)
+        bitmap.getPixels(intValues, 0, width, 0, 0, width, height)
+        var pixel = 0
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                val value = intValues[pixel++]
+
+                // Normalize channel values to [-1.0, 1.0]. This requirement varies by
+                // model. For example, some models might require values to be normalized
+                // to the range [0.0, 1.0] instead.
+                inputImage.putFloat(((value shr 16 and 0xFF) - mean) / std)
+                inputImage.putFloat(((value shr 8 and 0xFF) - mean) / std)
+                inputImage.putFloat(((value and 0xFF) - mean) / std)
+            }
+        }
+
+        inputImage.rewind()
+        return inputImage
+    }
+
+    fun convertArrayToBitmap(
+        imageArray: Array<Array<Array<FloatArray>>>,
+        imageWidth: Int,
+        imageHeight: Int
+    ): Bitmap {
+        val conf = Bitmap.Config.ARGB_8888 // see other conf types
+        val styledImage = Bitmap.createBitmap(imageWidth, imageHeight, conf)
+
+        for (x in imageArray[0].indices) {
+            for (y in imageArray[0][0].indices) {
+                val color = Color.rgb(
+                    ((imageArray[0][x][y][0] * 255).toInt()),
+                    ((imageArray[0][x][y][1] * 255).toInt()),
+                    (imageArray[0][x][y][2] * 255).toInt()
+                )
+
+                // this y, x is in the correct order!!!
+                styledImage.setPixel(y, x, color)
+            }
+        }
+        return styledImage
     }
 }
